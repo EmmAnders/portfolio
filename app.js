a59810abc603c3413118b735776bc57f3c05ec1f
Emma <emmandersson166@gmail.com>
2021-02-23 08:21:49 +0100

    -

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 
-gsap.from("header", { duration: 1, opacity: 0, delay: 0.8 });
-gsap.from(".intro", { duration: 1, opacity: 0, delay: 0.9 });
-gsap.from(".selected-projects", { duration: 1, opacity: 0, delay: 1 });
-gsap.from(".projects", { duration: 1, opacity: 0, delay: 1.1 });
+gsap.from("header", { duration: 1, y:'50', opacity: 0, delay: 0.6 });
+gsap.from(".intro", { duration: 1, y:'50', opacity: 0, delay: 0.9 });
+gsap.from(".selected-projects", { duration: 1, y:'50', opacity: 0, delay: 1.3 });
+gsap.from(".projects", { duration: 1, y:'50', opacity: 0, delay: 1.6 });
 
 
-// Cursor
-let mouse = document.querySelector(".cursor");
 
-function cursor(e) {
-  mouse.style.top = e.pageY + "px";
-  mouse.style.left = e.pageX + "px";
-}
-
-window.addEventListener("mousemove", cursor);
